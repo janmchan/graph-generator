157a79ed2cadde7f5267402b50b7962308fb86fd
janmchan <chanjma@icloud.com>
2018-06-17 09:40:46 +1200

    Updated index.js to new graph

--- index.js
+++ index.js
@@ -3,45 +3,7 @@
 const ChartjsNode = require('chartjs-node');
 // 600x600 canvas size
 var chartNode = new ChartjsNode(600, 600);
-
-var horizontalBarChartData = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-    }]
-};
-
-var chartJsOptions = {
-    type: 'bar',
-    data: horizontalBarChartData,
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-};
+var chartJsOptions = demoOptions();
 
 return chartNode.drawChart(chartJsOptions)
 .then(() => {
@@ -80,3 +42,69 @@ return chartNode.drawChart(chartJsOptions)
     // ./testimage.png
 });
 
+function demoOptions()
+{
+    var horizontalBarChartData = demoData();
+    return {
+        type: 'bar',
+        data: {
+            labels: ["2018-06-11", "2018-06-12", "2018-06-13", "2018-06-14", "2018-06-15", "2018-06-16", "2018-06-17"],
+            datasets: [ {
+                label: "Symbicort Actuals",
+                type: "bar",
+                yAxesGroup: "1",
+                backgroundColor: "rgba(255,0,0,.75)",
+                data: [4, 5, 3, 2, 4, 4, 2]
+            },
+            {
+                label: "Ventolin Actuals",
+                type: "bar",
+                yAxesGroup: "1",
+                backgroundColor: "rgba(0,0,255,.75)",
+                data: [1, 1, 0, 1, 0, 0, 2]
+            },{
+                label: "Symbicort Prescribed",
+                type: "line",
+                yAxesGroup: "2",
+                borderColor: "rgba(255,0,0,1)",
+                borderWidth: 1,
+                data: [4, 4, 4, 4, 4, 4, 4]
+            },{
+                label: "Ventolin Prescribed",
+                type: "line",
+                yAxesGroup: "2",
+                borderColor: "rgba(0,0,255,1)",
+                borderWidth: 1,data: [0, 0, 0, 0, 0, 0, 0]
+            }],
+            yAxes: [{
+                name: "1",
+                scalePositionLeft: false,
+                scaleFontColor: "rgba(151,137,200,0.8)"
+            }, {
+                name: "2",
+                scalePositionLeft: true,
+                scaleFontColor: "rgba(151,187,205,0.8)"
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                },
+            ],
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'day',
+                    unitStepSize: 1,
+                    displayFormats: {
+                    'day': 'MMM DD'
+                    }
+                }
+            }]
+            }
+        }
+    };
+};
